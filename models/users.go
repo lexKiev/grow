fa9f950488a98d289cadb310ea1a50bbe76b3c15
Rudycode <46867584+Rudycode@users.noreply.github.com>
2019-05-20 21:44:46 +0300

    Add files via upload

--- models/users.go
+++ models/users.go
@@ -2,11 +2,12 @@ package models
 
 
 type User struct {
+	Id string
 	Login string
 	Email string
 	Password string
 }
 
-func NewUser(login, email, password string) *User {
-	return &User{login, email, password}
+func NewUser(id, login, email, password string) *User {
+	return &User{id,login, email, password}
 }
